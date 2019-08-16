7a0e8367f151b53afeaf27761c1908d1e53476dd
Maha Saad <maha.saad.mohamed@gmail.com>
2019-08-16 09:31:24 +0200

    refactor  - convert to arrow function

--- startup/routes.js
+++ startup/routes.js
@@ -6,12 +6,11 @@ const auth = require('../middleware/auth')
 const errHandler = require('../middleware/errorhandler')
 
 module.exports = (app) => {
-    app.use(express.json());
+    app.use(express.json())
+
     app.use('/api/users', users);
     app.use('/api/ingredients', auth, ingredients);
     app.use('/api/diseases', auth, diseases);
     app.use(errHandler.handle)
-    app.use(function(req, res, next) {
-        res.status(404).send('Not found');
-    });
+    app.use((req, res, next) => { res.status(404).send('Not found'); });
 }
