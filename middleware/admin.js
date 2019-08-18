ebdefa2d2a8be217691e0dfe5e94b97d889bbf68
Maha Saad <maha.saad.mohamed@gmail.com>
2019-08-18 15:42:41 +0200

    refactor-rename

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,6 @@
 const { Roles } = require('../models/user')
 module.exports = (req, res, next) => {
     if (req.user.roles.indexOf(Roles.Admin) === -1)
-        return res.status(403).send('Acess denied');
+        return res.status(403).send('Access denied');
     next();
 }
